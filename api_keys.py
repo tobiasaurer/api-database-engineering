57224b181f4d59e39d77f2d9f80de731a01833c3
tobiasaurer <tobias@aurer.de>
2022-08-03 15:54:18 +0200

    initialize repo

---
+++ api_keys.py
@@ -0,0 +1,10 @@
+"""
+Main keys for api connections
+"""
+
+# Key for openweather API (https://openweathermap.org/)
+openweather_api_key = ''
+
+# Key and host for aerodatabox API from x-rapid (https://rapidapi.com/aedbx-aedbx/api/aerodatabox/)
+aerodatabox_api_key = ''
+aerodatabox_host = ''
